1cf75a32253f54fe2b2fb7750221559fea1edf90
e2tha-e <ee@electric-eloquence.io>
2016-12-17 08:57:23 -0500

    install.js reads from conf.yml

--- install.js
+++ install.js
@@ -2,14 +2,34 @@
 
 const fs = require('fs');
 
-const appDir = '../../../app';
+const rootDir = `${process.cwd()}/../../..`;
+const confFile = `${rootDir}/conf.yml`;
 const rcFile = '.htmllintrc';
 
-if (fs.existsSync(appDir)) {
-  fs.readFile(rcFile, (err, data) => {
+if (fs.existsSync(confFile)) {
+  fs.readFile(confFile, (err, data) => {
     if (err) {
       throw err;
     }
-    fs.writeFile(`${appDir}/${rcFile}`, data);
+
+    // app_dir should be the first config.
+    let appDir = `${rootDir}/`;
+    let regex = /^[^\n]*/;
+
+    if (regex.test(data)) {
+      appDir += data.match(regex)[0].trim();
+    }
+    else {
+      appDir += 'app';
+    }
+
+    if (fs.existsSync(appDir)) {
+      fs.readFile(rcFile, (err1, data1) => {
+        if (err1) {
+          throw err1;
+        }
+        fs.writeFile(`${appDir}/${rcFile}`, data1);
+      });
+    }
   });
 }
