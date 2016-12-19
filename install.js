1909a5c4eeaf061dc86e0682d597b9c772622d5e
e2tha-e <ee@electric-eloquence.io>
2016-12-18 19:38:38 -0500

    probably works now

--- install.js
+++ install.js
@@ -11,35 +11,11 @@ if (!filepath) {
 }
 
 const rootDir = path.dirname(filepath);
-const confFile = `${rootDir}/conf.yml`;
 const rcFile = process.argv[2];
-console.info('rootDir');
-console.info(rootDir);
 
-if (fs.existsSync(confFile)) {
-  fs.readFile(confFile, {encoding: 'utf8'}, (err, data) => {
-    if (err) {
-      throw err;
-    }
-
-    let appDir = `${rootDir}/`;
-    let regex = /^app_dir\s*:.*$/m;
-
-    if (regex.test(data)) {
-      let appDirConf = data.match(regex)[0].trim().split(':');
-      appDir += appDirConf[1].trim();
-    }
-    else {
-      appDir += 'app';
-    }
-
-    if (fs.existsSync(appDir)) {
-      fs.readFile(rcFile, (err1, data1) => {
-        if (err1) {
-          throw err1;
-        }
-        fs.writeFile(`${appDir}/${rcFile}`, data1);
-      });
-    }
-  });
-}
+fs.readFile(rcFile, (err1, data1) => {
+  if (err1) {
+    throw err1;
+  }
+  fs.writeFile(`${rootDir}/${rcFile}`, data1);
+});
