d8fb05c726032013142f143a8b06b79fbea69629
e2tha-e <ee@electric-eloquence.io>
2019-07-06 12:14:20 -0400

    added fp htmllint:help task

--- htmllint~extend.js
+++ htmllint~extend.js
@@ -2,6 +2,7 @@
 
 const gulp = global.gulp || require('gulp');
 const htmllint = require('gulp-htmllint');
+const utils = require('fepper-utils');
 
 const {
   conf,
@@ -16,4 +17,20 @@ pref.htmllint.config = pref.htmllint.config || `${rootDir}/.htmllintrc`;
 gulp.task('htmllint', function () {
   return gulp.src(conf.ui.paths.public.patterns + '/*/!(index|viewall|*markup-only).html')
     .pipe(htmllint(pref.htmllint));
+});
+
+gulp.task('htmllint:help', function (cb) {
+  let out = `
+Fepper Htmlint Extension
+
+Use:
+    <task> [<additional args>...]
+
+Tasks:
+    fp htmllint         Lint Fepper's frontend HTML files.
+    fp htmllint:help    Print fp-htmllint tasks and descriptions.
+`;
+
+  utils.info(out);
+  cb();
 });
